1545c9104f11071f38e17f1f9637c156f55f9b89
Ritik Mathur <1824433@brunel.ac.uk>
2019-10-23 23:33:58 +0100

    Building UI commit

--- app/src/main/java/com/example/bullscows/MainActivity.java
+++ app/src/main/java/com/example/bullscows/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bullscows;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"WELCOME^_^",Toast.LENGTH_SHORT).show();
+
+
     }
+
 }
